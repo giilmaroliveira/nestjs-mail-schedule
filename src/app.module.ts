34b9b319055d60cb0404ec5a24304bc698cd9277
Gilmar Oliveira <giilmaroliveira@outlook.com>
2022-11-30 01:31:32 -0300

    chore: install @nestjs/typeorm, typeorm and pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SendgridModule } from './app/sendgrid/sendgrid.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), SendgridModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      syncronize: true,
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+    }),
+    SendgridModule,
+  ],
   controllers: [],
   providers: [],
 })

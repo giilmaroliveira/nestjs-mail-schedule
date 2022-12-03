566c63eceebae916226fba2358c9a55944af6821
Gilmar Oliveira <giilmaroliveira@outlook.com>
2022-12-03 13:02:26 -0300

    feat: add MailCron

--- src/app/mail/mail.service.spec.ts
+++ src/app/mail/mail.service.spec.ts
@@ -1,13 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { FindAllMailDto } from './dto/find-all-mail.dto';
 import { SaveMailDto } from './dto/save-mail.dto';
+import { MailStatusEnum } from './enum/mail-status.enum';
 import { MailEntity } from './mail.entity';
 import { MailService } from './mail.service';
 
 describe('MailService', () => {
   let mailService: MailService;
   let mailRepository: Repository<MailEntity>;
+  const getMany = jest.fn();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,6 +21,11 @@ describe('MailService', () => {
           useValue: {
             create: jest.fn(),
             save: jest.fn(),
+            createQueryBuilder: jest.fn().mockReturnThis(),
+            andWhere: jest.fn(),
+            getMany,
+            findOneOrFail: jest.fn(),
+            merge: jest.fn(),
           },
         },
       ],
@@ -30,6 +38,47 @@ describe('MailService', () => {
   it('should be defined', () => {
     expect(mailService).toBeDefined();
     expect(mailRepository).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should retrun a mail list with success', async () => {
+      // Arrange
+      const mailEntityMockList = [
+        { id: '1', dueDate: '2022-11-15T12:00:00Z' },
+        { id: '2', dueDate: '2022-11-30T12:00:00Z' },
+      ] as MailEntity[];
+      getMany.mockResolvedValueOnce(mailEntityMockList);
+      // Act
+      const result = await mailService.findAll();
+      // Assert
+      expect(result).toHaveLength(2);
+    });
+
+    it('should return a filtered mail list with dueDateLte', async () => {
+      // Arrange
+      const mailEntityMockList = [{ id: '1', dueDate: '2022-11-15T12:00:00Z' }] as MailEntity[];
+      const params: Partial<FindAllMailDto> = { dueDateLte: '2022-11-20T12:00:00Z' };
+      getMany.mockResolvedValueOnce(mailEntityMockList);
+      // Act
+      const result = await mailService.findAll(params);
+      // Assert
+      expect(result).toHaveLength(1);
+    });
+
+    it('should return a filtered mail list with WAITING status', async () => {
+      // Arrange
+      const mailEntityMockList = [{ id: '1', dueDate: '2022-11-15T12:00:00Z' }] as MailEntity[];
+      const params: Partial<FindAllMailDto> = { status: MailStatusEnum.WAITING };
+      getMany.mockResolvedValueOnce(mailEntityMockList);
+      // Act
+      const result = await mailService.findAll(params);
+      // Assert
+      expect(result).toHaveLength(1);
+    });
+  });
+
+  afterEach(() => {
+    getMany.mockRestore();
   });
 
   describe('save', () => {
@@ -53,6 +102,17 @@ describe('MailService', () => {
       expect(result).toBeDefined();
       expect(mailRepository.create).toBeCalledTimes(1);
       expect(mailRepository.save).toBeCalledTimes(1);
+    });
+  });
+
+  describe('updateStatus', () => {
+    it('should update mail status with success', async () => {
+      // Arrange
+      const id = '1';
+      // Act
+      const result = await mailService.updateStatus(id, MailStatusEnum.SENT);
+      // Assert
+      expect(result).toBeUndefined();
     });
   });
 });
